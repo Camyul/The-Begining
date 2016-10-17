0719c7daa7562daab9bf3fdf4368dd035fffc961
Camyul <vankata.lt@outlook.com>
2016-10-17 22:32:36 +0300

    Complete Problem 03.01

---
+++ 03. Operators and Expressions/01. Odd or Even/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        sbyte n = sbyte.Parse(Console.ReadLine());
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("even {0}", n);
+        }
+        else
+        {
+            Console.WriteLine("odd {0}", n);
+
+        }
+    }
+}
