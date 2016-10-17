b277b5a8335be2e5826ff97ce22d20f9fb989a98
Camyul <vankata.lt@outlook.com>
2016-10-18 00:38:32 +0300

    Complete Problem 03.02

---
+++ 03. Operators and Expressions/02.  Moon Gravity/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double wight = Convert.ToDouble(Console.ReadLine());
+        double moonWight = wight * 0.17;
+        Console.WriteLine("{0:F3}", moonWight);
+    }
+}
