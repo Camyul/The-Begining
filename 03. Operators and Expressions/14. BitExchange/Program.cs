20186f21846237aa7aeda137137c6349c625d369
Camyul <vankata.lt@outlook.com>
2016-10-20 09:39:37 +0300

    Complete Homework 03. Operators and Expressions

---
+++ 03. Operators and Expressions/14. BitExchange/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        uint n = Convert.ToUInt32(Console.ReadLine());
+        uint[] bits = new uint[6];
+        for (int i = 0; i < 3; i++)
+        {
+            int mask = 1 << (i + 3);
+            bits[i] = (n & (uint)mask) >> (i + 3);
+        }
+        for (int i = 3; i < 6; i++)
+        {
+            int mask = 1 << (i + 21);
+            bits[i] = (n & (uint)mask) >> (i + 21);
+        }
+        //Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
+        for (int i = 0; i < 3; i++)
+        {
+            if (bits[i] > 0)
+            {
+                int mask = (int)bits[i] << i + 24;
+                n |= (uint)mask;
+            }
+            else
+            {
+                int mask = ~(1 << i + 24);
+                n &= (uint)mask;
+            }
+        }
+        for (int i = 3; i < 6; i++)
+        {
+            if (bits[i] > 0)
+            {
+                int mask = (int)bits[i] << i;
+                n |= (uint)mask;
+            }
+            else
+            {
+                int mask = ~(1 << i);
+                n &= (uint)mask;
+            }
+        }
+        //Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
+        Console.WriteLine(n);
+    }
+}
---
+++ 03. Operators and Expressions/15. BitSwap/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        uint n = uint.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+        int q = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+        uint[] bitP = new uint[k];
+        uint[] bitQ = new uint[k];
+        int maskP = 1;
+        int maskQ = 1;
+        for (int i = 0; i < k; i++)
+        {
+            maskP = 1 << (p + i);
+            bitP[i] = (n & (uint)maskP) >> (p + i);
+            maskQ = 1 << (q + i);
+            bitQ[i] = (n & (uint)maskQ) >> (q + i);
+        }
+        for (int i = 0; i < k; i++)
+        {
+            if (bitP[i] > 0)
+            {
+                maskP = 1 << q + i;
+                n |= (uint)maskP;
+            }
+            else
+            {
+                maskP = ~(1 << q + i);
+                n &= (uint)maskP;
+            }
+            if (bitQ[i] > 0)
+            {
+                maskQ = 1 << p + i;
+                n |= (uint)maskQ;
+            }
+            else
+            {
+                maskQ = ~(1 << p + i);
+                n &= (uint)maskQ;
+            }
+        }
+        Console.WriteLine(n);
+    }
+}
