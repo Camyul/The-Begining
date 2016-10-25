f569e0e231326312e75b200f187d381023bd4ac7
Camyul <vankata.lt@outlook.com>
2016-10-26 01:15:34 +0300

    Complete Problem 05.09

---
+++ 05. Conditional Statements/09. Int, Double and String/09. Int, Double and String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string typeOfVariable = Console.ReadLine();
+        if (typeOfVariable == "integer")
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number + 1);
+        }
+        else if (typeOfVariable == "real")
+        {
+            double number = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:F2}", number + 1);
+        }
+        else
+        {
+            string sequence = Console.ReadLine();
+            Console.WriteLine(sequence + "*");
+        }
+    }
+}
