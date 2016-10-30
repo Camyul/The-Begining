52f03f3fd1ce79582150612932e73af38df210c9
Camyul <vankata.lt@outlook.com>
2016-10-30 22:02:13 +0200

    Complete Problem 06.17

--- 06. Loops/17. Spiral Matrix/17. Spiral Matrix.cs
+++ 06. Loops/17. Spiral Matrix/17. Spiral Matrix.cs
@@ -5,16 +5,62 @@ class Program
     {
         int num = int.Parse(Console.ReadLine());
         int[,] numbers = new int[num, num];
-        int col = 1;
-        int row = 1;
+        int col = 0;
+        int row = 0;
         string direction = "Right";
-        for (int i = 0; i < num; i++)
+        for (int i = 1; i <= num * num; i++)
         {
+            if ((direction == "Right" && col >num -1) || (direction == "Right" && numbers[row,col] > 0))
+            {
+                direction = "Down";
+                col--;
+                row++;
+            }
+            if ((direction == "Down" && row > num -1) || (direction == "Down" && numbers[row, col] > 0))
+            {
+                direction = "Left";
+                row--;
+                col--;
+            }
+            if ((direction == "Left" && col < 0) || (direction == "Left" && numbers[row, col] > 0))
+            {
+                direction = "Up";
+                col++;
+                row--;
+            }
+            if ((direction == "Up" && row < 0) || (direction == "Up" && numbers[row, col] > 0))
+            {
+                direction = "Right";
+                row++;
+                col++;
+            }
+
+            numbers[row,col] = i;
+
             if (direction == "Right")
             {
-
+                col++;
             }
-            Console.WriteLine(num);
+            else if (direction == "Down")
+            {
+                row++;
+            }
+            else if (direction == "Left")
+            {
+                col--;
+            }
+            else
+            {
+                row--;
+            }
+        }
+        for (row = 0;  row < num; row++)
+        {
+            for (col = 0; col < num; col++)
+            {
+                Console.Write("{0} ", numbers[row, col]);
+            }
+            Console.WriteLine();
         }
     }
 }
