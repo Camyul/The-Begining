de21665790ceafc3674485b790e79954b1f5ba1f
Camyul <vankata.lt@outlook.com>
2016-11-03 01:05:29 +0200

    Start Problem 07.11

---
+++ 07. Arrays/10. Find sum in array/10. Find sum in array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string sequence = Console.ReadLine();
+        string[] arr = sequence.Split(' ');
+        int[] intArr = new int[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            intArr[i] = int.Parse(arr[i]);
+        }
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum = 0;
+            for (int j = i; j < arr.Length; j++)
+            {
+                sum += intArr[j];
+                if (sum < 11)
+                {
+                    continue;
+                }
+                else if (sum > 11)
+                {
+                    break;
+                }
+                else
+                {
+                    for (int k = i; k <= j; k++)
+                    {
+                        Console.Write("{0} ", intArr[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ 07. Arrays/11. Binary search/11. Binary search.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            num[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int x = Convert.ToInt32(Console.ReadLine());
+    }
+}
