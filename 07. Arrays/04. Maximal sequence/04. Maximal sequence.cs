84c0a60c22dab1b722d1dc8480e7afb2068be7f3
Camyul <vankata.lt@outlook.com>
2016-11-02 11:24:02 +0200

    Start Problem 07.05

---
+++ 07. Arrays/04. Maximal sequence/04. Maximal sequence.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] num = new int[n];
+        int count = 1;
+        int maxCount = 1;
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            num[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        if ((n == 2) && (num[0] == num[1]))
+        {
+            maxCount++;
+        }
+        while (j < n - 1)
+        {
+            if (num[j + 1] == num[j])
+            {
+                count++;
+            }
+            if (num[j + 1] != num[j])
+            {
+                if (maxCount < count)
+                {
+                    maxCount = count;
+                }
+                count = 1;
+            }
+            j++;
+        }
+        if (maxCount < count)
+        {
+            maxCount = count;
+        }
+        Console.WriteLine(maxCount);
+    }
+}
