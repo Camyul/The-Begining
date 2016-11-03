955ef26664687ba5b9140a1b2db04fa10c5eedab
Camyul <vankata.lt@outlook.com>
2016-11-03 11:30:55 +0200

    Working on Problem 07.11 - 80/100

--- 07. Arrays/11. Binary search/11. Binary search.cs
+++ 07. Arrays/11. Binary search/11. Binary search.cs
@@ -4,11 +4,37 @@ class Program
     static void Main()
     {
         int n = Convert.ToInt32(Console.ReadLine());
-        int[] num = new int[n];
+        int[] nums = new int[n];
         for (int i = 0; i < n; i++)
         {
-            num[i] = Convert.ToInt32(Console.ReadLine());
+            nums[i] = Convert.ToInt32(Console.ReadLine());
         }
-        int x = Convert.ToInt32(Console.ReadLine());
+        int target = Convert.ToInt32(Console.ReadLine());
+        int first = 0;
+        int mid = 0;
+        int last = nums.Length - 1;
+        bool found = false;
+        while (!found && first <= last )
+        {
+            mid = first + (last - first) / 2;
+            if (target == nums[mid])
+            {
+                found = true;
+                Console.WriteLine(mid);
+                return;
+            }
+            else
+            {
+                if (target > nums[mid])
+                {
+                    first = mid + 1;
+                }
+               else
+                {
+                    last = mid - 1;
+                }
+            }
+        }
+        Console.WriteLine(nums[first] >= target ? first : last);
     }
 }
