70c0a4f552ae9e656681b6d25802723d2bddb8fc
Camyul <vankata.lt@outlook.com>
2016-11-05 20:24:37 +0200

    Complete Problem 07.17

---
+++ 07. Arrays/14. Quick sort v.2/14. Quick sort v.2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        QuickSort(numbers, 0, numbers.Length - 1);
+
+        foreach (var nums in numbers)
+        {
+            Console.WriteLine(nums);
+        }
+    }
+
+    public static void QuickSort(int[] numbs, int left, int right)
+    {
+        
+        if (left < right)
+        {
+            int part = Partition(numbs, left, right);
+            QuickSort(numbs, left, part - 1);
+            QuickSort(numbs, part + 1, right);
+        }
+    }
+    private static int Partition(int[] nums, int begin, int end)
+    {
+        int pivot = nums[end];
+        int temp = 0;
+        int start = begin;
+        for (int i = begin; i < end; i++)
+        {
+            if (nums[i] <= pivot)
+            {
+                temp = nums[i];
+                nums[i] = nums[start];
+                nums[start] = temp;
+                start++;
+            }
+        }
+        nums[end] = nums[start];
+        nums[start] = pivot;
+        return start;
+    }
+}
---
+++ 07. Arrays/17. Subset K with sum S/17. Subset K with sum S.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string number = Console.ReadLine();
+        string[] numbers = number.Split(' ');
+        int[] nums = new int[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            nums[i] = int.Parse(numbers[i]);
+        }
+        int k = int.Parse(Console.ReadLine());
+        int sum = int.Parse(Console.ReadLine());
+        int tempSum = 0;
+        string avelibleSum = "no";
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+            for (int j = i; j <= k; j++)
+            {
+
+                if (tempSum < sum)
+                {
+                    tempSum += nums[j];
+                }
+                else if (tempSum == sum)
+                {
+                    avelibleSum = "yes";
+                }
+                else
+                {
+                    tempSum = 0;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(avelibleSum);
+    }
+}
