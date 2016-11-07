dfe6c761c4122e98f124e3305578f5836a87da9d
Camyul <vankata.lt@outlook.com>
2016-11-07 15:16:54 +0200

    Bobby Avokadoto

---
+++ 09. Workshop Exam/5. Bobby Avokadoto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        uint head = uint.Parse(Console.ReadLine());
+        uint combCount = uint.Parse(Console.ReadLine());
+        uint bestComb = 0;
+        uint bestCount = 0;
+
+        for (int i = 0; i < combCount; i++)
+        {
+            uint comb = uint.Parse(Console.ReadLine());
+            if ((head & comb) == 0)
+            {//comb is valid
+                //count teech
+                uint currentCount = 0;
+                for (int j = 0; j < 32; j++)
+                {
+                    currentCount += (comb>>j) &  1;
+
+                }
+                //check if best
+                if (currentCount > bestCount)
+                {
+                    bestCount = currentCount;
+                    bestComb = comb;
+                }
+            }
+        }
+        Console.WriteLine(bestComb);
+    }
+}
